abcdf9a01e772621841a211de3525c04cb97d495
baljotsinghh <95204807+baljotsinghh@users.noreply.github.com>
2025-05-11 13:50:30 +0530

    Update gemini_wrapper.py

--- gemini_wrapper.py
+++ gemini_wrapper.py
@@ -4,7 +4,7 @@ import requests
 
 class GeminiAPI:
     def __init__(self):
-        self.api_key = os.getenv("GEMINI_API_KEY")
+        self.api_key = st.secrets("GEMINI_API_KEY")
         if not self.api_key:
             raise ValueError("GEMINI_API_KEY not found in environment variables.")
         self.url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key={self.api_key}"
